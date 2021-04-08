dd3571f79e650192f8e69e609d4b62591d8e5fd8
Wesley_Wong <Wesley_Wong@asus.com>
2021-04-08 18:31:36 +0800

    add basic action and reducer files
    Change-Id: I02ab7b0cd24aae04b612c93bb43521cc026a223e

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunkMiddleware)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/pages/homePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = (props) => {
+    const {
+    } = props;
+
+    const rootDOM = React.useRef(); // current container ref
+    
+    React.useEffect(() => {
+        
+    }, []);
+
+};
+
+export default HomePage;
